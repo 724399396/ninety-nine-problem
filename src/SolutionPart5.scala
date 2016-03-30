1cd56e37b453112dba8e2d05a7322eea22a900b4
liww.li <liww.li@qunar.com>
2016-03-30 18:38:19 +0800

    update

--- src/SolutionPart5.scala
+++ src/SolutionPart5.scala
@@ -3,6 +3,17 @@ object SolutionPart5 extends App {
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def nodeCount: Int = 1 + children.map(_.nodeCount).sum
 
+    def internalPathLength: Int = children.foldLeft(0)((acc,x) => acc + x.nodeCount + x.internalPathLength)
+
+    def postorder: List[T] = children.flatMap(_.postorder) ++ List(value)
+
+    def lispyTree: String =
+      if (children.length > 0) {
+        " (" + value + children.map(_.lispyTree).mkString(" ") + ")"
+      } else {
+        " " + value.toString
+      }
+
     override def toString = value.toString + children.map(_.toString).mkString + "^"
   }
 
@@ -10,12 +21,22 @@ object SolutionPart5 extends App {
     def apply[T](value: T) = new MTree(value, List())
 
     def string2MTree(value: String): MTree[Char] = {
-      def help(list: List[Char]): List[MTree[Char]] = list match {
-        case x :: '^' :: Nil => List(MTree(x))
-        case x :: xs => List(MTree(x, help(xs)))
+      def help(start: Int): List[String] = {
+        if (start == value.length - 1) Nil
+        else {
+          val end = nextBound(start+1, 1)
+          value.substring(start, end) :: help(end)
+        }
       }
 
-      help(value.toCharArray.toList).apply(0)
+      def nextBound(pos: Int, nesting: Int): Int = {
+        if (nesting == 0)
+          pos
+        else
+          nextBound(pos+1, if (value(pos) == '^') nesting - 1 else nesting + 1)
+      }
+
+      MTree(value(0), help(1).map(string2MTree))
     }
   }
 
@@ -28,4 +49,8 @@ object SolutionPart5 extends App {
   println(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString)
 
   println(transfer("afg^^c^bd^e^^^"))
+
+  println("afg^^c^bd^e^^^".internalPathLength)
+  println("afg^^c^bd^e^^^".postorder)
+  println(MTree("a", List(MTree("b", List(MTree("c"))))).lispyTree)
 }
---
+++ src/SolutionPart6.scala
@@ -0,0 +1,122 @@
+/**
+  * @author liww.li
+  * @date 2016-03-30 15:28
+  */
+object SolutionPart6 extends App {
+  abstract class GraphBase[T, U] {
+    case class Edge(n1: Node, n2: Node, value: U) {
+      def toTuple = (n1.value, n2.value, value)
+    }
+
+    case class Node(value: T) {
+      var adj: List[Edge] = Nil
+      def neighbors: List[Node] = adj.map(edgeTarget(_,this).get)
+    }
+
+    var nodes: Map[T, Node] = Map()
+    var edges: List[Edge] = Nil
+
+    def edgeTarget(e: Edge, n: Node): Option[Node]
+
+    override def equals(o: Any) = o match {
+      case g: GraphBase[T,U] => (nodes.keys.toSet -- g.nodes.keys.toSet == Nil &&
+                                 edges.map(_.toTuple).toSet -- g.edges.map(_.toTuple).toSet == Nil)
+      case _ => false
+    }
+
+    def addNode(value: T) = {
+      val n = new Node(value)
+      nodes = Map(value -> n) ++ nodes
+    }
+  }
+
+  class Graph[T, U] extends GraphBase[T, U] {
+    override def equals(o: Any) = o match {
+      case g: Graph[T, U] => super.equals(o)
+      case _ => false
+    }
+
+    def edgeTarget(e: Edge, n: Node) = {
+      if (e.n1 == n) Some(e.n2)
+      else if (e.n2 == n) Some(e.n1)
+      else None
+    }
+
+    def addEdge(n1: T, n2: T, value: U) = {
+      val e = new Edge(new Node(n1), new Node(n2), value)
+      edges = e :: edges
+      nodes(n1).adj = e :: nodes(n1).adj
+      nodes(n2).adj = e :: nodes(n2).adj
+    }
+
+    def toTermForm: (List[T], List[(T,T,U)]) = {
+      (nodes.keys.toList, edges.map(_.toTuple))
+    }
+  }
+
+  object Graph {
+    def fromString(str: String): Graph[String, Unit] = {
+      val res = new Graph[String, Unit]
+      str.drop(1).dropRight(1).split(",").foreach {
+        _.split("-") match {
+          case Array(n1, n2) =>
+            res.addNode(n1.trim)
+            res.addNode(n2.trim)
+            res.addEdge(n1.trim, n2.trim, ())
+          case Array(n) =>
+            res.addNode(n.trim)
+        }
+      }
+      res
+    }
+  }
+
+  class DiGraph[T, U] extends GraphBase[T, U] {
+    override def equals(o: Any) = o match {
+      case g: Graph[T, U] => super.equals(o)
+      case _ => false
+    }
+
+    def edgeTarget(e: Edge, n: Node) = {
+      if (e.n1 == n) Some(e.n2)
+      else None
+    }
+
+    def addArc(source: T, dest: T, value: U) = {
+      val e = new Edge(new Node(source), new Node(dest), value)
+      edges = e :: edges
+      nodes(source).adj = e :: nodes(source).adj
+    }
+
+    def toAdjacentForm: List[(T,List[(T,U)])] = {
+      nodes.values.toList.map{node =>
+        (node.value, node.adj.map(x => (x.n2.value, x.value)))
+      }
+    }
+  }
+
+  object Digraph {
+    def fromStringLabel(str: String): DiGraph[String, Int] = {
+      val res = new DiGraph[String,Int]()
+      str.drop(1).dropRight(1).split(",").foreach {
+        _.split(">") match {
+          case Array(n1, y) =>
+            y.split("/") match {
+              case Array(n2, w) => {
+                res.addNode(n1.trim)
+                res.addNode(n2.trim)
+                res.addArc(n1.trim, n2.trim, w.toInt)
+              }
+            }
+          case Array(n) =>
+            res.addNode(n.trim)
+        }
+      }
+      res
+    }
+  }
+
+  println(Graph.fromString("[b-c, f-c, g-h, d, f-b, k-f, h-g]").toTermForm)
+
+  println(Digraph.fromStringLabel("[p>q/9, m>q/7, k, p>m/5]").toAdjacentForm)
+}
